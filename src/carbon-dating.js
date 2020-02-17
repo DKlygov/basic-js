c51567def1e87940fd2692df4207b015f12699ef
DKlygov <dklygov@gmail.com>
2020-02-17 17:29:15 +0600

    feat: added solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity != 'string'|| !parseFloat(sampleActivity) || !parseInt(sampleActivity) || sampleActivity < 0 || sampleActivity > 15) {
+        return false;
+    }else{
+        sampleActivity = sampleActivity.replace(/^([^\.]*\.)|\./g, '$1' );
+        var rateConstant = 0.693/HALF_LIFE_PERIOD;
+        var initByFinal = MODERN_ACTIVITY/sampleActivity;
+        var age = Math.ceil(Math.log(initByFinal)/rateConstant);
+
+    }
+    return age;
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    var turnsResult = Math.pow(2, disksNumber) - 1;
+    var secondsResult = turnsResult / (turnsSpeed/3600);
+    var result = {
+        turns: turnsResult,
+        seconds: secondsResult
+    };
+    return result;
 }
