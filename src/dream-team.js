23045195d6ce5e714c696626ad0c279269cfa9df
DKlygov <dklygov@gmail.com>
2020-03-19 17:51:13 +0600

    feat: added solution to dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+  let teamNameArr = [];
+  for (var i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      teamNameArr.push(members[i].trim().charAt(0).toUpperCase());
+    }
+  };
+
+  return teamNameArr.sort().join("");
+}
